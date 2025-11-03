23dd6352ad4f69dc083aadf5d6d0ed18bcc8626e
federico.mastandrea <federico.mastandrea@motork.io>
2025-11-03 16:07:17 +0100

    Update GitHub Pages configuration for fedemastan35.github.io

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/bookS/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [react()],
   test: {
     globals: true,
